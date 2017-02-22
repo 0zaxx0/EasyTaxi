cd1e7f2c28c882b0abc9a6eae73fc80d2541eff0
Matias <0zaxx0@gmail.com>
2017-02-22 10:39:50 -0300

    AppBienvenidos

--- test1/archivos/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-def nombre(instance, filename):
-    return 'user_/{0}'.format(filename)
-class Document(models.Model):
-    docfile=models.FileField(upload_to=nombre)
-# Create your models here.
--- test1/winn/ExcelParser.py
+++
@@ -1,5 +0,0 @@
-import pyexcel
-
-class ExcelParser():
-    def read_excel(self):
-        #print (type(sheet))
